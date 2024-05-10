e532317249107ae764ba01677cdab2c92748b7d1
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2024-05-10 12:34:54 +0200

    feat(test): Actualiza el index con la nueva disposición del objeto fireboose, y lo testea exitosamente

--- test/index.js
+++ test/index.js
@@ -6,12 +6,47 @@ import firebooseConnectionSettings from '../runtime/config/firebase-config.json'
 
 fireboose.connect(firebooseConnectionSettings);
 
-const Trial = fireboose.model('Trial', 'trials')
+const Pet = fireboose.model('Pet', 'pets');
+const createPet = async function (info) {
+  const id = info.name;
+  info.id = id
+  await this.insertOne(id, info)
 
-const expectedName = 'Trial';
-const actualName = Trial.name;
+  return info;
+}
+Pet.createPet = createPet;
+
+
+// Test 1
+const expectedName = 'Pet';
+const actualName = Pet.name;
 if (expectedName == actualName) {
   console.debug('[Success] Test of name value')
 } else {
   console.debug('[Failure] Test of name value')
 }
+
+if (fireboose.db) {
+  console.debug('[Success] Test of db value')
+} else {
+  console.debug('[Failure] Test of db value')
+}
+
+
+// Test 2
+Pet.createPet({name: 'jero'})
+  .then(function(resolve) {
+    console.debug('insertOneResolve', resolve);
+    return resolve;
+  })
+  .then(function(resolve) {
+    Pet.findOneById(resolve.id)
+      .then(function(resolve) {
+        console.debug('findOneByIdResolve', resolve);
+      })
+  })
+  .catch(function(reject) {
+    console.error('Error in Test 2!');
+    console.error(reject);
+  })
+
