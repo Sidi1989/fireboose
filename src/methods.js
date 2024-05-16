c880e7cb1d64359d77f2e72ff249356766ce49c1
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2024-05-16 11:41:03 +0200

    feat(methods): Modifica largamente los methods, sus descriptions y returns

--- src/methods.js
+++ src/methods.js
@@ -1,7 +1,8 @@
 import { flatten } from 'flat';
 import { 
   collection, doc,
-  setDoc, getDoc, getDocs, 
+  addDoc, setDoc, 
+  getDoc, getDocs, 
   updateDoc, deleteDoc,
   arrayUnion, arrayRemove,
   query, where, limit,
@@ -12,39 +13,30 @@ import {
 
 /**
  * @description
- * Función asíncrona para recuperar varios documents consignados 
- * en una collection (con un límite al número de ellos)
+ * Async function to retrieve multiple documents from a colelction
+ * (with an optional limit in their retrieval)
  * @returns Array (of firestore docs)
  */
 const findMany = async function () {
   const db = this.db;
   const collectionName = this.collection;
-  if (!db || !collectionName) {
-    console.warn('[WARNING] [methods/findMany] Not enough params');
-    throw new Error();
-  }
-  
   const collectionRef = collection(db, collectionName);
+  
+  // Set the number of documents to be retrieved
   const queryLimit = limit(10);
   const queryDocs = query(collectionRef, queryLimit);
 
-  try {
-    var querySnap = await getDocs(queryDocs);
-    const docs = [];
-    querySnap.forEach((doc) => docs.push(doc.data()));
-    return docs;
-  } catch (e) {
-    console.error(e);
-    return [];
-  }
+  var querySnap = await getDocs(queryDocs);
+  const docs = [];
+  querySnap.forEach((doc) => docs.push(doc.data()));
+  return docs;
 };
 
 
 /**
  * @description
- * Función asíncrona para recuperar un document consignado 
- * en una collection, no sólo por su Id, sino por cualquier 
- * propiedad que contenga
+ * Async function to retrieve a document from a collection, known by any of
+ * its attributes
  * @param {String} propertyName E.g: age
  * @param {} propertyValue E.g: 23
  * @returns Object (firestore doc) || null
@@ -52,60 +44,51 @@ const findMany = async function () {
 const findOneByProperty = async function (propertyName, propertyValue) {
   const db = this.db;
   const collectionName = this.collection;
-  if (!db || !collectionName || !propertyName || !propertyValue) {
-    console.warn('[WARNING] [methods/findOneByProperty] Not enough params');
-    throw new Error();
+  const collectionRef = collection(db, collectionName);
+
+  if (!propertyName || !propertyValue) {
+    throw new Error('Not enough params for [findOneByProperty]')
   }
   
-  const collectionRef = collection(db, collectionName);
+  // Here is set the attribute to be searched for
   const queryConditions = where(propertyName, '==', propertyValue);
   const queryDocs = query(collectionRef, queryConditions);
 
-  try {
-    const docs = [];
-    var querySnap = await getDocs(queryDocs);
-    querySnap.forEach((doc) => docs.push(doc.data()));
-    
-    if (docs.length > 0) {
-      return docs[0];
-    } else {
-      return null
-    }
-  } catch (e) {
-    console.error(e);
-    return null;
+  const docs = [];
+  var querySnap = await getDocs(queryDocs);
+  querySnap.forEach((doc) => docs.push(doc.data()));
+  
+  if (docs.length > 0) {
+    return docs[0];
+  } else {
+    return null
   }
 };
 
 
 /**
  * @description
- * Función asíncrona para recuperar un document consignado 
- * en una collection
+ * Async function to retrieve a document from a collection, known by
+ * its ID
  * @param {String} docId E.g: m1a
  * @returns Object (firestore doc) || null
  */
 const findOneById = async function (docId) {
   const db = this.db;
   const collectionName = this.collection;
-  if (!db || !collectionName || !docId) {
-    console.warn('[WARNING] [methods/findOneById] Not enough params');
-    throw new Error();
-  }
-
   const collectionRef = collection(db, collectionName); 
-  const docRef = doc(collectionRef, docId);
 
-  try {
-    var docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      const doc = docSnap.data();
-      return doc;
-    } else {
-      return null;
-    }
-  } catch (e) {
-    console.error(e);
+  if (!docId) {
+    throw new Error('Not enough params for [findOneById]')
+  }
+
+  const docRef = doc(collectionRef, docId);
+  var docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    const doc = docSnap.data();
+    return doc;
+  } else {
     return null;
   }
 };
@@ -113,169 +96,171 @@ const findOneById = async function (docId) {
 
 /**
  * @description
- * Función asíncrona para añadir un document a una collection
- * @param {String} docId E.g: m1a
+ * Async function to create / add a document to a collection
  * @param {Object} docInfo E.g: {name: Miguel, username: Gobo}
- * @returns String || null
+ * @param {String} docId (Optional) E.g: m1a
+ * @return String
  */
-const insertOne = async function (docId, docInfo) {
+const create = async function (docInfo, docId) {
   const db = this.db;
   const collectionName = this.collection;
-  if (!db || !collectionName || !docId || !docInfo) {
-    console.warn('[WARNING] [methods/insertOne] Not enough params');
-    throw new Error();
-  }
-
   const collectionRef = collection(db, collectionName);
-  const docRef = doc(collectionRef, docId);
 
-  try {
-    await setDoc(docRef, docInfo);
-    return docId;
-  } catch (e) {
-    console.error(e);
-    return null;
+  if (!docInfo) { 
+    throw new Error('Not enough params for [create]') 
   }
-};
+
+  if (docId) {
+    const docRef = doc(collectionRef, docId);
+    await setDoc(docRef, docInfo);
+    return docRef.id;
+  // If there isn't a meaningful ID for the document, let Cloud Firestore 
+  // auto-generate an ID
+  } else {
+    const docRef = await addDoc(collectionRef, docInfo);
+    return docRef.id;
+  }
+}
 
 
 /**
  * @description
- * Función asíncrona para sobreescribir un document de una collection
+ * Async function to create a document in a collection
+ * @param {Object} docInfo E.g: {name: Miguel, username: Gobo}
  * @param {String} docId E.g: m1a
- * @param {Object} docInfo E.g: {name: Fernando, username: Fer}
- * @returns String || null
+ * @returns String
  */
-const updateOne = async function (docId, docInfo) {
+const insertOne = async function (docInfo, docId) {
   const db = this.db;
   const collectionName = this.collection;
-  if (!db || !collectionName || !docId || !docInfo) {
-    console.warn('[WARNING] [methods/updateOne] Not enough params');
-    throw new Error();
+  const collectionRef = collection(db, collectionName);
+
+  if (!docInfo || !docId) {
+    throw new Error('Not enough params for [insertOne]')
   }
 
-  const collectionRef = collection(db, collectionName);  
   const docRef = doc(collectionRef, docId);
-
-  try {
-    // If the info to update is contained in a nested object like this:
-    // {
-    //   name: "Miguel", 
-    //   surnames: {
-    //     first: "Díaz", 
-    //     last: "Laclaustra"
-    //   }
-    // }
-    // And the update (docInfo) is represented like this:
-    // {
-    //   surnames: {
-    //     first: Janssen
-    //   }
-    // }
-    // 'flatten' will transform the object in a 'firebase dot notation' like this:
-    //   {"surnames.first": "Janssen"} 
-    await updateDoc(docRef, flatten(docInfo));
-    return docId;
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
+  await setDoc(docRef, docInfo);
+  return docId;
 };
 
 
 /**
  * @description
- * Función asíncrona para eliminar un document consignado 
- * en una collection
+ * Async function to overwrite a document in a collection
+ * @param {Object} docInfo E.g: {name: Fernando, username: Fer}
  * @param {String} docId E.g: m1a
- * @returns String || null
+ * @returns String
+ */
+const updateOne = async function (docInfo, docId) {
+  const db = this.db;
+  const collectionName = this.collection;
+  const collectionRef = collection(db, collectionName); 
+
+  if (!docInfo || !docId) {
+    throw new Error('Not enough params for [updateOne]')
+  }
+
+  const docRef = doc(collectionRef, docId);
+  await updateDoc(docRef, flatten(docInfo));
+
+  // About 'nested objects':
+
+  // If the info to update is contained in a nested object like this:
+  // {
+  //   name: "Miguel", 
+  //   surnames: {
+  //     first: "Díaz", 
+  //     last: "Laclaustra"
+  //   }
+  // }
+
+  // And the update (docInfo) is represented like this:
+  // {
+  //   surnames: {
+  //     first: Janssen
+  //   }
+  // }
+
+  // 'flatten' will transform the object in a 'firebase dot notation' like this:
+  //   {"surnames.first": "Janssen"}
+
+  return docId;
+};
+
+
+/**
+ * @description
+ * Async function to delete a document from its collection
+ * @param {String} docId E.g: m1a
+ * @returns String
  */
 const deleteOne = async function (docId) {
   const db = this.db;
   const collectionName = this.collection;
-  if (!db || !collectionName || !docId) {
-    console.warn('[WARNING] [methods/deleteOne] Not enough params');
-    throw new Error();
-  }
-
   const collectionRef = collection(db, collectionName); 
-  const docRef = doc(collectionRef, docId);
 
-  try {
-    await deleteDoc(docRef);
-    return docId;
-  } catch (e) {
-    console.error(e);
-    return null;
+  if (!docId) {
+    throw new Error('Not enough params for [deleteOne]')
   }
+
+  const docRef = doc(collectionRef, docId);
+  await deleteDoc(docRef);
+  return docId;
 };
 
 
 /**
  * @description
- * Función asíncrona para actualizar un document consignado 
- * en una collection respecto de una de sus propiedades tipo array,
- * añadiéndola un elemento
+ * Async function to update a document in a collection, concerning one of 
+ * its array attributes, in which new elements are included
  * @param {String} docId E.g: m1a
- * @param {String} arrayToUpdate E.g: shkleps
- * @param {Mixed} fieldInfo 46 || 'Miguel' || {name: Miguel, userRole: admin}
- * @returns String || null
+ * @param {String} arrayToUpdate E.g: countries
+ * @param {Mixed} fieldInfo 46 || 'Spain' || {name: Spain, city: Madrid}
+ * @returns String
  */
 const updateArrayByAddingOne = async function (docId, arrayToUpdate, fieldInfo) {
   const db = this.db;
   const collectionName = this.collection;
-  if (!db || !collectionName || !docId || !arrayToUpdate || !fieldInfo) {
-    console.warn('[WARNING] [methods/updateArrayByAddingOne] Not enough params');
-    throw new Error();
+  const collectionRef = collection(db, collectionName);
+
+  if (!docId || !arrayToUpdate || !fieldInfo) {
+    throw new Error('Not enough params for [updateArrayByAddingOne]')
   }
 
-  const collectionRef = collection(db, collectionName); 
   const docRef = doc(collectionRef, docId);
-
-  try {
-    let updateInfo = {
-      [arrayToUpdate]: arrayUnion(fieldInfo)
-    }
-    await updateDoc(docRef, updateInfo);
-    return docId;
-  } catch (e) {
-    console.error(e);
-    return null;
+  let updateInfo = {
+    [arrayToUpdate]: arrayUnion(fieldInfo)
   }
+  await updateDoc(docRef, updateInfo);
+  return docId;
 };
 
 
 /**
  * @description
- * Función asíncrona para actualizar un document consignado 
- * en una collection respecto de una de sus propiedades tipo array,
- * quitándola uno de sus elementos
+ * Async function to update a document in a collection, concerning one of 
+ * its array attributes, in which new elements are excluded
  * @param {String} docId E.g: m1a
- * @param {String} arrayToUpdate E.g: shkleps
- * @param {Mixed} fieldInfo 46 || 'Miguel' || {name: Miguel, userRole: admin}
+ * @param {String} arrayToUpdate E.g: countries
+ * @param {Mixed} fieldInfo 46 || 'Spain' || {name: Spain, city: Madrid}
  * @returns String || null
  */
 const updateArrayByRemovingOne = async function (docId, arrayToUpdate, fieldInfo) {
   const db = this.db;
   const collectionName = this.collection;
-  if (!db || !collectionName ||!docId || !arrayToUpdate || !fieldInfo) {
-    console.warn('[WARNING] [methods/updateArrayByRemovingOne] Not enough params');
-    throw new Error();
+  const collectionRef = collection(db, collectionName); 
+
+  if (!docId || !arrayToUpdate || !fieldInfo) {
+    throw new Error('Not enough params for [updateArrayByRemovingOne]')
   }
   
-  const collectionRef = collection(db, collectionName); 
   const docRef = doc(collectionRef, docId);
-
-  try {
-    let updateInfo = {
-      [arrayToUpdate]: arrayRemove(fieldInfo)
-    }
-    await updateDoc(docRef, updateInfo);
-    return docId;
-  } catch (e) {
-    console.error(e);
-    return null;
+  let updateInfo = {
+    [arrayToUpdate]: arrayRemove(fieldInfo)
   }
+  await updateDoc(docRef, updateInfo);
+  return docId;
 };
 
 
@@ -285,6 +270,7 @@ export {
   findMany,
   findOneById,
   findOneByProperty,
+  create,
   insertOne,
   updateOne,
   deleteOne,
