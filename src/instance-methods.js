45915fbf8c38d7fc922919b579788906e413ccce
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2024-05-16 19:00:31 +0200

    feat(methods): Añade y mejora los instance-methods

--- src/instance-methods.js
+++ src/instance-methods.js
@@ -19,7 +19,7 @@ const save = async function () {
   const collectionRef = collection(db, collectionName);
 
   const docRef = doc(collectionRef, this.__id);
-  await setDoc(docRef, this.info);
+  await setDoc(docRef, this.toObject());
   return this.__id;
 };
 
@@ -27,10 +27,32 @@ const save = async function () {
 /**
  * @description
  * Function to help logging information about the instance
+ * 
+ * It includes both inherited and owned properties
+ * Its result will be coincident to the .toObject() instance method
  */
-const inspect = async function () {
-  console.log('Info about this instance of:', this)
-  console.log('And its instance methods:', this.__proto__)
+const inspect = function () {
+  for (let prop of Object.getOwnPropertyNames(this)) {
+    console.log(prop);
+  }
+};
+
+
+/**
+ * @description
+ * Function to convert an instance into a plain object
+ * 
+ * It includes only ownPropertyNames (both enumerable and non-enumerable),
+ * but not inherited properties, since we want to exclude methods like:
+ * save(), inspect(),... which are inherited from its parent Class
+ */
+const toObject = function () {
+  const pojo = {}
+  for (let prop of Object.getOwnPropertyNames(this)) {
+    pojo[prop] = this[prop];
+  }
+
+  return pojo;
 }
 
 
@@ -39,4 +61,5 @@ const inspect = async function () {
 export {
   save,
   inspect,
+  toObject,
 }
