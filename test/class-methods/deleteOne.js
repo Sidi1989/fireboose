233f1644151f89008be9bc1d58e5bf50281a56a4
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2024-05-21 13:44:37 +0200

    feat(create & delete methods): Testea, explica y corrige largamente los methods para crear y eliminar Firestore Docs

--- test/class-methods/deleteOne.js
+++ test/class-methods/deleteOne.js
@@ -21,33 +21,23 @@ const countrySchemaConfig = {};
 const countrySchema = new Schema(countrySchemaDefinition, countrySchemaConfig);
 const Country = fireboose.model('Country', countrySchema, 'countries');
 
-// Test 1
-Country.create({name: 'Spain', capital: 'Madrid'}, 'createTestId1')
+// Test
+Country.create({name: 'Spain', capital: 'Madrid'}, 'deleteOneTestId1');
+
+Country.deleteOne('deleteOneTestId1')
   .then(function(resolve) {
-    let expectedResolve = 'createTestId1';
+    let expectedResolve = 'deleteOneTestId1';
     if (expectedResolve != resolve) {
-      console.error('Failure at first promise of .create() in Test 1');
+      console.error('Failure at first promise of .deleteOne()');
     }
     return resolve;
   })
   .then(function(resolve) {
     Country.findOneById(resolve)
       .then(function(resolve) {
-        let expectedResolve = {name: 'Spain', capital: 'Madrid'};
-        if (!_.isEqual(expectedResolve, resolve)) {
-          console.error('Failure at second promise of .create() in Test 1');
-        }
-      })
-  })
-
-// Test 2
-Country.create({name: 'France', capital: 'Paris'})
-  .then(function(resolve) {
-    Country.findOneById(resolve)
-      .then(function(resolve) {
-        let expectedResolve = {name: 'France', capital: 'Paris'};
-        if (!_.isEqual(expectedResolve, resolve)) {
-          console.error('Failure at first promise of .create() in Test 2');
+        let expectedResolve = null;
+        if (expectedResolve != resolve) {
+          console.error('Failure at second promise of .deleteOne()');
         }
       })
   })
--- test/class-methods/newClass.js
+++ test/class-methods/newClass.js
@@ -1,4 +1,4 @@
-import fireboose from '../../src/index.js';
+import fireboose, {Schema} from '../../src/index.js';
 import firebooseConnectionSettings from '../../runtime/config/firebase-config.json' assert { type: "json" };
 
 
@@ -6,19 +6,34 @@ import firebooseConnectionSettings from '../../runtime/config/firebase-config.js
 
 fireboose.connect(firebooseConnectionSettings);
 
-const Pet = fireboose.model('Pet', 'pets');
+const countrySchemaDefinition = {
+  name: {
+    $type: String,
+    required: true
+  },
+  capital: {
+    $type: String,
+    required: true
+  },
+};
+const countrySchemaConfig = {};
+const countrySchema = new Schema(countrySchemaDefinition, countrySchemaConfig);
+const Country = fireboose.model('Country', countrySchema, 'countries');
 
 // Test
-const expectedName = 'Pet';
-const actualName = Pet.name;
-if (expectedName == actualName) {
-  console.debug('[Success] Test of name value')
-} else {
-  console.debug('[Failure] Test of name value')
+if (!fireboose.db) {
+  console.error('Failure at new Class DbTest')
 }
 
-if (fireboose.db) {
-  console.debug('[Success] Test of db value')
-} else {
-  console.debug('[Failure] Test of db value')
+const actualCollection = Country.collection;
+const expectedCollection = 'countries';
+if (expectedCollection != actualCollection) {
+  console.error('Failure at new Class CollectionTest')
 }
+
+const actualName = Country.name;
+const expectedName = 'Country';
+if (expectedName != actualName) {
+  console.error('Failure at new Class NameTest')
+}
+
